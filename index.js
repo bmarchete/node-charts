76d86ee9b437a788c628d689553f1c4263997d25
bmarchete <bmarchete@gmail.com>
2018-03-08 01:23:01 -0300

    video 5

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var knex = require("knex");
 const path = require('path');
-
-const db = knex({
-    client: "mysql",
-    connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "",
-      database: "db"
-    }
-  });
+const routes = require('./routes/index');
 
 const app = express();
 
@@ -21,24 +11,8 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false}));
 
-app.get('/', (req, res) =>{
-    
-    res.render('index');
-})
+app.use('/', routes);
 
-app.get('/all', (req, res, next) =>{
-    db('charts').then(dados => {
-        res.send(dados);
-      }, next);
-})
-
-app.post('/save', (req, res, next) =>{
-    db('charts')
-    .insert(req.body)
-    .then(dados => {
-      return res.send(dados);
-    }, next);
-})
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
